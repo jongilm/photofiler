a50ff65a2dd16c2b4bf3394c6bef93842c548fb8
Jonathan Gilmore <jonathan@jgilmore.eu>
2019-12-28 22:23:18 +0000

    Initial checkin - reading exif ok, traversing tree needs more testing

--- PhotoFiler/Program.cs
+++ PhotoFiler/Program.cs
@@ -1,15 +1,154 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhotoFiler
 {
     class Program
     {
+        public static bool fVerbose = false;
+        public static bool fRecursive = true;
+        public static bool fBrandedCamerasOnly = true;
+        public static bool fUnbrandedCamerasOnly = false;
+        public static bool fKnownCamerasOnly = true;
+        public static bool fUnknownCamerasOnly = true;
+
+        public static int numberOfFilesProcessed = 0;
+        public static int numberOfFilesShown = 0;
+        public static int numberOfFilesFiltered = 0;
+
         static void Main( string[] args )
         {
+            try
+            {
+                string filePath = @".";
+                //filePath = "20191130_192659_IMG_5030.JPG";
+                //filePath = @"C:\\Users\\Jonnie\\source\\repos\\PhotoFiler\\PhotoFiler\\bin\\Debug";
+                //filePath = @"C:/Users/Jonnie/source/repos/PhotoFiler/PhotoFiler/bin/Debug";
+                if (args.Length > 0)
+                {
+                    filePath = args[0];
+                }
+                if (fVerbose)
+                    Console.WriteLine("Processing images in \"" + filePath + "\"");
+
+                DirectorySearch(filePath, 0, fRecursive, true);
+                Console.WriteLine("numberOfFilesProcessed = " + numberOfFilesProcessed );
+                Console.WriteLine("numberOfFilesShown     = " + numberOfFilesShown );
+                Console.WriteLine("numberOfFilesFiltered  = " + numberOfFilesFiltered );
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"FATAL: {ex}");
+            }
+            if (fVerbose)
+            {
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            return;
+        }
+
+
+        public static void DirectorySearch(string root, int depth, bool recursive = true, bool processRootDirectoryFiles = true )
+        {
+            if (processRootDirectoryFiles)
+            {
+                Console.WriteLine(depth + ": Files in \"" + root + "\" ...");
+                var rootDirectoryFiles = Directory.GetFiles(root);
+                foreach (var file in rootDirectoryFiles)
+                {
+                    ProcessImage(file);
+                } 
+            }
+            if (recursive)
+            { 
+                Console.WriteLine(depth + ": Folders in \"" + root + "\" ...");
+                var subDirectories = Directory.GetDirectories(root);
+                if (subDirectories?.Any() == true)
+                {
+                    foreach (var directory in subDirectories)
+                    {
+                        var files = Directory.GetFiles(directory);
+                        foreach (var file in files)
+                        {
+                            ProcessImage(file);
+                        }
+                        DirectorySearch(directory, depth+1, true, false);
+                    }
+                }
+            }
+        }
+
+        static void ProcessImage( string filePath )
+        {
+            if (filePath.EndsWith(".jpg",StringComparison.OrdinalIgnoreCase) == false)
+                return;
+
+            if (fVerbose)
+                Console.WriteLine("Processing: [" + filePath + "] ...");
+
+            PhotoFiler.ExifData exif1 = new ExifData(filePath);
+
+            numberOfFilesProcessed++;
+            if (fBrandedCamerasOnly && (exif1.cameraMake == "" || exif1.cameraModel == ""))
+            {
+                numberOfFilesFiltered++;
+                return;
+            }
+
+            if (fUnbrandedCamerasOnly && (exif1.cameraMake != "" || exif1.cameraModel != ""))
+            {
+                numberOfFilesFiltered++;
+                return;
+            }
+
+            numberOfFilesShown++;
+
+            if (fVerbose)
+            {
+                Console.WriteLine("   cameraMake: " + exif1.cameraMake);
+                Console.WriteLine("   cameraModel: " + exif1.cameraModel);
+                //Console.WriteLine("   lensMake: " + exif1.lensMake);
+                //Console.WriteLine("   lensModel: " + exif1.lensModel);
+                Console.WriteLine("   imageHeight: " + exif1.imageHeight);
+                Console.WriteLine("   imageWidth: " + exif1.imageWidth);
+                Console.WriteLine("   datetimeOriginal: " + exif1.datetimeOriginalString);
+                Console.WriteLine("   imageFiletime: " + exif1.imageFiletimeString);
+                Console.WriteLine("   imageFilesize: " + exif1.imageFilesize);
+                Console.WriteLine("   imageFilename: " + exif1.imageFilename);
+            }
+            else
+            {
+                double megapixels = (double)(exif1.imageHeight * exif1.imageWidth)/(1024*1024);
+                
+                Console.WriteLine(  Truncate(exif1.cameraMake,20) + "\t" +
+                                    Truncate(exif1.cameraModel,20) + "\t" +
+                                    exif1.imageHeight + "\t" + 
+                                    exif1.imageWidth + "\t" + 
+                                    String.Format("{0,8:0.0000}", megapixels) + "\t" + 
+                                    exif1.datetimeOriginalString + "\t" + 
+                                    exif1.imageFiletimeString + "\t" + 
+                                    exif1.imageFilesize + "\t" + 
+                                    exif1.imageFilename);
+            }
+
+        }
+
+        public static string Truncate(string source, int length)
+        {
+            if (source.Length > length)
+            {
+                source = source.Substring(0, length);
+            }
+            else if (source.Length < length)
+            {
+                source = source.PadRight(length);
+            }
+            return source;
         }
     }
-}
+
+} 
+
---
+++ PhotoFiler/exifData.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+// https://github.com/drewnoakes/metadata-extractor/wiki/GettingStarted
+using MetadataExtractor;
+using MetadataExtractor.Formats.Exif;
+using MetadataExtractor.Formats.Iptc;
+using MetadataExtractor.Formats.Jpeg;
+
+#pragma warning disable 8321
+
+
+
+namespace PhotoFiler
+{
+    internal class ExifData
+    {
+        // [JPEG] Image Height - 4000 pixels
+        // [JPEG] Image Width - 6000 pixels
+        // [Exif IFD0] Make - Canon
+        // [Exif IFD0] Model - Canon EOS 750D
+        // [Exif IFD0] Orientation - Top, left side (Horizontal / normal)
+        // [Exif IFD0] Date/Time - 2019:11:30 19:26:59
+        // [Exif SubIFD] Date/Time Original - 2019:11:30 19:26:59
+        // [Exif SubIFD] Exif Image Width - 6000 pixels
+        // [Exif SubIFD] Exif Image Height - 4000 pixels
+        // [Exif SubIFD] Camera Owner Name -
+        // [Exif SubIFD] Body Serial Number - 283072008309
+        // [Exif SubIFD] Lens Model - TAMRON 18-200mm F/3.5-6.3 DiII VC B018
+        // [Exif SubIFD] Lens Serial Number - 0000000000
+        // [File Type] Detected File Type Name - JPEG
+        // [File Type] Expected File Name Extension - jpg
+        // [File] File Name - 20191130_192659_IMG_5030.JPG
+        // [File] File Size - 8434775 bytes
+
+        public string filePath { get; set; }
+
+        // Attributes
+        public int imageWidth { get; set; }
+        public int imageHeight { get; set; }
+        public string imageWidthDescription { get; set; }
+        public string imageHeightDescription { get; set; }
+        public string cameraMake { get; set; }
+        public string cameraModel { get; set; }
+        public string lensMake { get; set; }
+        public string lensModel { get; set; }
+        public System.DateTime? datetimeOriginal { get; set; }
+        public string datetimeOriginalString { get; set; }
+        public string imageFilename { get; set; }
+        public long imageFilesize { get; set; }
+        public System.DateTime imageFiletime { get; set; }
+        public string imageFiletimeString { get; set; }
+        
+
+        public ExifData(string filename)
+        {
+            filePath = filename;
+
+            //Console.WriteLine($"Processing file: {filePath}");
+
+            try
+            {
+                // This will transparently determine the file type and invoke the appropriate
+                // readers. This will handle JPEG, TIFF, GIF, BMP and RAW (CRW/CR2/NEF/RW2/ORF) files
+                // and extract whatever metadata is available and understood.
+                var directories = ImageMetadataReader.ReadMetadata(filePath);
+                if (directories == null) return;
+
+                //PrintListOfDirectories(directories);
+                //PrintAllTags(directories, "Using ImageMetadataReader");
+                GetPhotoMetadata(directories);
+            }
+            catch (ImageProcessingException e)
+            {
+                PrintError(e);
+            }
+            catch (IOException e)
+            {
+                PrintError(e);
+            }
+
+        }
+
+        void GetPhotoMetadata(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            imageHeight = this.GetImageHeight(directories);
+            imageWidth = this.GetImageWidth(directories);
+            datetimeOriginal = this.GetTakenDateTime(directories);
+            System.DateTime timestamp = (datetimeOriginal != null) ? (System.DateTime)datetimeOriginal: imageFiletime;
+            datetimeOriginalString = timestamp.ToString("yyyyMMdd'_'HHmmss");
+
+            cameraMake = this.GetCameraMake(directories);
+            cameraModel = this.GetCameraModel(directories);
+
+            //lensMake = this.GetLensMake(directories);
+            //lensModel = this.GetLensModel(directories);
+
+            imageFilename = filePath;
+            imageFilesize = GetFilesize(filePath);
+            imageFiletime = GetFiletime(filePath);
+            imageFiletimeString = imageFiletime.ToString("yyyyMMdd'_'HHmmss");
+        }
+
+        int GetImageWidth(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            int temp = 0;
+            // Obtain a specific directory
+            var directory = directories.OfType<JpegDirectory>().FirstOrDefault();
+            if (directory == null) return 0;
+            temp = directory.GetImageWidth();
+            return temp;
+        }
+
+        int GetImageHeight(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            int temp = 0;
+            // Obtain a specific directory
+            var directory = directories.OfType<JpegDirectory>().FirstOrDefault();
+            if (directory == null) return 0;
+            temp = directory.GetImageHeight();
+            return temp;
+        }
+
+        string GetImageWidthDescr(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            string temp = "";
+            // Obtain a specific directory
+            var directory = directories.OfType<JpegDirectory>().FirstOrDefault();
+            if (directory == null) return "";
+
+            // Create a descriptor
+            var descriptor = new JpegDescriptor(directory);
+            if (descriptor == null) return "";
+            // Get tag description
+            temp = descriptor.GetImageHeightDescription();
+            if (temp == null) return "";
+            return temp;
+        }
+
+        string GetImageHeightDescr(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            string temp = "";
+            // Obtain a specific directory
+            var directory = directories.OfType<JpegDirectory>().FirstOrDefault();
+            if (directory == null) return "";
+            // Create a descriptor
+            var descriptor = new JpegDescriptor(directory);
+            if (descriptor == null) return "";
+            // Get tag description
+            temp = descriptor.GetImageHeightDescription();
+            if (temp == null) return "";
+            return temp;
+        }
+
+        string GetCameraMake(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            int tagValue = ExifDirectoryBase.TagMake;
+            var directory = directories.OfType<ExifDirectoryBase>().FirstOrDefault();
+            if (directory == null) return "";
+            StringValue stringval = directory.GetStringValue(tagValue);
+            if (stringval.Bytes == null) return "";
+            string result = stringval.ToString();
+            if (result == null) return "";
+            return result;
+        }
+
+        string GetCameraModel(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            int tagValue = ExifDirectoryBase.TagModel;
+            var directory = directories.OfType<ExifDirectoryBase>().FirstOrDefault();
+            if (directory == null) return "";
+            StringValue stringval = directory.GetStringValue(tagValue);
+            if (stringval.Bytes == null) return "";
+            string result = stringval.ToString();
+            if (result == null) return "";
+            return result;
+        }
+
+        string GetLensMake(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            int tagValue = ExifDirectoryBase.TagLensMake;
+            var directory = directories.OfType<ExifDirectoryBase>().FirstOrDefault();
+            if (directory == null) return "";
+            StringValue stringval = directory.GetStringValue(tagValue);
+            if (stringval.Bytes == null) return "";
+            string result = stringval.ToString();
+            if (result == null) return "";
+            return result;
+        }
+
+        string GetLensModel(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            int tagValue = ExifDirectoryBase.TagLensModel;
+            var directory = directories.OfType<ExifDirectoryBase>().FirstOrDefault();
+            if (directory == null) return "";
+            StringValue stringval = directory.GetStringValue(tagValue);
+            if (stringval.Bytes == null) return "";
+            string result = stringval.ToString();
+            if (result == null) return "";
+            return result;
+        }
+
+        DateTime? GetTakenDateTime(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            // Obtain the Exif SubIFD directory
+            var directory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
+            if (directory == null) return null;
+            // Query the tag's value
+            if (directory.TryGetDateTime(ExifDirectoryBase.TagDateTimeOriginal, out var dateTime))
+                return dateTime;
+            return null;
+        }
+
+        string GetExposureProgramDescription(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            // Obtain a specific directory
+            var directory = directories.OfType<ExifSubIfdDirectory>().FirstOrDefault();
+            if (directory == null) return null;
+            // Create a descriptor
+            var descriptor = new ExifSubIfdDescriptor(directory);
+            if (descriptor == null) return null;
+            // Get tag description
+            return descriptor.GetExposureProgramDescription();
+        }
+
+        long GetFilesize(string filePath)
+        {
+            System.IO.FileInfo f = new System.IO.FileInfo(filePath);
+            if (f == null) return -1;
+            long temp = f.Length;
+            return temp;
+        }
+
+        System.DateTime GetFiletime(string filePath)
+        {
+            System.IO.FileInfo f = new System.IO.FileInfo(filePath);
+            if (f == null) return System.DateTime.Now;
+            System.DateTime temp = f.CreationTime;
+            return temp;
+        }
+
+        void PrintListOfDirectories(IEnumerable<MetadataExtractor.Directory> directories)
+        {
+            // Extraction gives us potentially many directories
+            foreach (var directory in directories)
+            {
+                Console.WriteLine("   directory.Name: " + directory.Name + " ***");
+                // Each directory may also contain error messages
+                foreach (var error in directory.Errors)
+                {
+                    Console.Error.WriteLine("   ERROR: " + error);
+                }
+            }
+        }
+
+        // Write all extracted values to stdout
+        void PrintAllTags(IEnumerable<MetadataExtractor.Directory> directories, string method)
+        {
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------");
+            Console.Write(' '); Console.WriteLine(method);
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine();
+
+            // Extraction gives us potentially many directories
+            foreach (var directory in directories)
+            {
+                Console.WriteLine("*********************************** directory.Name: " + directory.Name + " ***");
+                // Each directory stores values in tags
+                foreach (var tag in directory.Tags)
+                {
+                    Console.WriteLine(tag);
+                }
+
+                // Each directory may also contain error messages
+                foreach (var error in directory.Errors)
+                {
+                    Console.Error.WriteLine("ERROR: " + error);
+                }
+            }
+        }
+
+        void PrintError(Exception exception) => Console.Error.WriteLine($"EXCEPTION: {exception}");
+    };
+}
+
